dc3041e5a4e83b4ebe22229bf69c8482af3748ee
ngminhduc1612 <85179226+ngminhduc1612@users.noreply.github.com>
2024-04-04 16:58:07 +0700

    Animation for search screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,9 @@
-import { FlatList, } from "react-native";
+import { FlatList, Animated, LayoutChangeEvent } from "react-native";
+import { useState } from "react";
+
 import { Screen } from "@/components/Screen";
 import { Card } from "@/components/Card";
-import { LISTMARGIN } from "@/constants";
+import { HEADERHEIGHT, LISTMARGIN } from "@/constants";
 
 export default function SearchScreen() {
     const properties = [{
@@ -37,15 +39,136 @@ export default function SearchScreen() {
         state: "Floria",
         zip: 33137,
         tags: ["Parking"]
+    },
+    {
+        id: 3,
+        images: [
+            "https://visaho.vn/upload_images/images/2022/04/01/dien-tich-can-ho-chung-cu-2-min.jpg",
+            "https://vcdn-kinhdoanh.vnecdn.net/2019/12/23/image002-8881-1577094450.png"
+        ],
+        rentLow: 3750,
+        rentHigh: 31054,
+        bedroomLow: 1,
+        bedroomHigh: 5,
+        name: "The Duly 2",
+        street: "Hola 35th St",
+        city: "Miami",
+        state: "Floria",
+        zip: 33137,
+        tags: ["Parking"]
+    },
+    {
+        id: 4,
+        images: [
+            "https://visaho.vn/upload_images/images/2022/04/01/dien-tich-can-ho-chung-cu-2-min.jpg",
+            "https://vcdn-kinhdoanh.vnecdn.net/2019/12/23/image002-8881-1577094450.png"
+        ],
+        rentLow: 3750,
+        rentHigh: 31054,
+        bedroomLow: 1,
+        bedroomHigh: 5,
+        name: "The Duly 2",
+        street: "Hola 35th St",
+        city: "Miami",
+        state: "Floria",
+        zip: 33137,
+        tags: ["Parking"]
+    },
+    {
+        id: 5,
+        images: [
+            "https://visaho.vn/upload_images/images/2022/04/01/dien-tich-can-ho-chung-cu-2-min.jpg",
+            "https://vcdn-kinhdoanh.vnecdn.net/2019/12/23/image002-8881-1577094450.png"
+        ],
+        rentLow: 3750,
+        rentHigh: 31054,
+        bedroomLow: 1,
+        bedroomHigh: 5,
+        name: "The Duly 2",
+        street: "Hola 35th St",
+        city: "Miami",
+        state: "Floria",
+        zip: 33137,
+        tags: ["Parking"]
     }
     ];
 
+    const [scrollAnimation] = useState(new Animated.Value(0));
+    const [offsetAnimation] = useState(new Animated.Value(0));
+    const [clampedScroll, setClampedScroll] = useState(
+        Animated.diffClamp(
+            Animated.add(
+                scrollAnimation.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [0, 1],
+                    extrapolateLeft: "clamp"
+                }),
+                offsetAnimation
+            ),
+            0,
+            1
+        )
+    );
+
+    const navbarTranslate = clampedScroll.interpolate({
+        inputRange: [0, HEADERHEIGHT],
+        outputRange: [0, -HEADERHEIGHT],
+        extrapolate: "clamp",
+    });
+
+    const onLayout = (event: LayoutChangeEvent) => {
+        let { height } = event.nativeEvent.layout
+        setClampedScroll(
+            Animated.diffClamp(
+                Animated.add(
+                    scrollAnimation.interpolate({
+                        inputRange: [0, 1],
+                        outputRange: [0, 1],
+                        extrapolateLeft: "clamp"
+                    }),
+                    offsetAnimation
+                ),
+                0,
+                height
+            )
+        )
+    }
+
     return (
-        <Screen style={{ marginHorizontal: LISTMARGIN }}>
-            <FlatList
+        <Screen>
+            <Animated.View
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    right: 0,
+                    left: 0,
+                    zIndex: 1000,
+                    height: HEADERHEIGHT,
+                    backgroundColor: "#fff",
+                    transform: [{ translateY: navbarTranslate }],
+                }}
+                onLayout={onLayout}
+            ></Animated.View>
+
+            <Animated.FlatList
+                onScroll={Animated.event([
+                    {
+                        nativeEvent: {
+                            contentOffset: {
+                                y: scrollAnimation,
+                            },
+                        },
+                    },
+                ],
+                    { useNativeDriver: true }
+                )}
+                contentContainerStyle={{paddingTop: HEADERHEIGHT + 20}}
+                bounces={false}
+                scrollEventThrottle={16}
                 data={properties}
                 keyExtractor={(item) => item.id.toString()}
                 showsVerticalScrollIndicator={false}
+                style={{ marginHorizontal: LISTMARGIN }}
                 renderItem={({ item }) => (
                     <Card style={{ marginVertical: 5 }} property={item} />
                 )}
