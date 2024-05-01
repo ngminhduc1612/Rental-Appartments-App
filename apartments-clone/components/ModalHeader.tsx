b7fc5e5e282fb0b5d4f3e93f7c3a7074d7a1a03d
ngminhduc1612 <85179226+ngminhduc1612@users.noreply.github.com>
2024-05-01 16:47:29 +0700

    Update findLocationScreen
    Combine with api to give guests the suggestions of places

---
+++ apartments-clone/components/ModalHeader.tsx
@@ -0,0 +1,65 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { View, StyleSheet, ViewStyle } from "react-native";
+import { Text } from "@ui-kitten/components";
+import { useNavigation } from "@react-navigation/native";
+
+import { theme } from "../theme";
+import { Row } from "./Row";
+
+export const ModalHeader = ({
+    XShown,
+    text,
+    style,
+}: {
+    XShown?: boolean;
+    text?: string;
+    style?: ViewStyle | ViewStyle[];
+}
+
+) => { 
+    const navigation = useNavigation();
+    
+    if (text) {
+        return (
+            <Row style={[styles.container, style as ViewStyle]}>
+                {XShown ? (
+                    <MaterialCommunityIcons
+                        onPress={navigation.goBack}
+                        style={styles.x}
+                        name="close"
+                        color={"black"}
+                        size={24}
+                    />
+                ) : null}
+                <Text category={"h5"}>{text}</Text>
+            </Row>
+        )
+    }
+
+    return (
+        <View style={[styles.container, style as ViewStyle]}>
+            <View style={styles.bar} />
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    x: {
+        position: "absolute",
+        left: 10,
+        alignSelf: "center"
+    },
+    container: {
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 15,
+        borderBottomWidth: 1,
+        borderBottomColor: "#a4a4a4",
+    },
+    bar: {
+        width: 50,
+        backgroundColor: "#a4a4a4",
+        height: 4,
+        borderRadius: 30,
+    }
+});
